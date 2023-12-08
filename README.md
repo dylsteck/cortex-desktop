285b1c9d231be9d62482b92786985955f3baf99c
dylsteck <dylancsteck@gmail.com>
2023-12-08 15:29:29 -0800

    saving progress
    got automerge added so gonna try to convert there

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# cortex
+# cortex-desktop
 
 Control all your apps in one place
 By [@dylsteck](https://dylansteck.com)
--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,10 +1,24 @@
+const path = require("path");
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const nextConfig = {
+  reactStrictMode: true,
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  webpack: (config) => {
-    return config
-  },
-}
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true
+    };
+    config.performance = {
+      hints: false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000
+    };
+    return config;
+  }
+};
+
+module.exports = nextConfig;
